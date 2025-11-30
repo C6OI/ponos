957e5f7b4f3c93feaf5cf3648f4db5238ce5929c
C6OI <c6oi@yandex.ru>
2025-11-30 06:14:15 +0500

    Implement GDT and Double Fault exception interrupt handler

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@
 pub mod serial;
 pub mod vga_buffer;
 pub mod interrupts;
+pub mod gdt;
 
 use core::panic::PanicInfo;
 
@@ -40,6 +41,7 @@ pub extern "C" fn _start() -> ! {
 }
 
 pub fn init() {
+    gdt::init(); 
     interrupts::init_idt();
 }
 
